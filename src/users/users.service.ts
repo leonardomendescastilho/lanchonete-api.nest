2f2725d21354ec23b348b9723deec47b9907d0e5
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-03 09:27:31 -0300

    feat: Implementa serviço que altera dado do usuário

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,9 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateUsersDto } from './users.dto';
+import { CreateUsersDto, UpdateUsersDto } from './users.dto';
 import { Users } from './users.entity';
+import { ObjectId } from 'mongodb';
 
 @Injectable()
 export class UsersService {
@@ -12,14 +13,44 @@ export class UsersService {
   ) {}
 
   async createUser(newUser: CreateUsersDto): Promise<Users> {
-    const userAlreadExists = await this.usersRepository.findOne({
-      where: { email: newUser.email },
-    });
+    try {
+      const userAlreadExists = await this.usersRepository.findOne({
+        where: { email: newUser.email },
+      });
 
-    if (userAlreadExists) throw new ConflictException('User already exists');
+      if (userAlreadExists) throw new ConflictException('User already exists');
 
-    const user = this.usersRepository.create(newUser);
-    console.log(user);
-    return await this.usersRepository.save(user);
+      const user = this.usersRepository.create(newUser);
+
+      return await this.usersRepository.save(user);
+    } catch (error) {
+      console.error('Error create user:', error);
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+      throw new ConflictException('Failed to create user');
+    }
+  }
+
+  async updateUser(user: UpdateUsersDto): Promise<void> {
+    try {
+      const userId = new ObjectId(user._id);
+      const userExists = await this.usersRepository.findOne({
+        where: { _id: userId },
+      });
+
+      if (!userExists) throw new ConflictException('User not found');
+
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { _id, ...updateUserData } = user;
+
+      await this.usersRepository.update(userId, updateUserData);
+    } catch (error) {
+      console.error('Error updating user:', error);
+      if (error instanceof ConflictException) {
+        throw error;
+      }
+      throw new ConflictException('Failed to update user');
+    }
   }
 }
