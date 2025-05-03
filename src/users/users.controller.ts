1d3bb3cf12452cd665087e839b089df46c397a0f
Leonardo Mendes Castilho <leonardo.smendescastilho@gmail.com>
2025-05-03 09:26:18 -0300

    feat: Implementa rota de update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,13 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUsersDto } from './users.dto';
+import { CreateUsersDto, UpdateUsersDto } from './users.dto';
 import { Users } from './users.entity';
 
 @Controller('users')
@@ -11,5 +18,11 @@ export class UsersController {
   @HttpCode(HttpStatus.CREATED)
   async createUser(@Body() newUser: CreateUsersDto): Promise<Users> {
     return await this.usersService.createUser(newUser);
+  }
+
+  @Patch('update')
+  @HttpCode(HttpStatus.OK)
+  async updateUser(@Body() user: UpdateUsersDto): Promise<void> {
+    return await this.usersService.updateUser(user);
   }
 }
